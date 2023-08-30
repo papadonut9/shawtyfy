fe39fc1748d31a2d38a1c2bf326bf3b04ef9a6d4
anchxt <47145773+papadonut9@users.noreply.github.com>
2023-08-30 23:07:09 +0530

    Store Service setup

--- store/store_service.go
+++ store/store_service.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/go-redis/redis"
-	"github.com/go-redis/redis/v8"
+	// "github.com/go-redis/redis/v8"
 )
 
 // Raw wrapper
@@ -20,6 +20,25 @@ var (
 	ctx          = context.Background()
 )
 
-
 // Cache expiration duration
 const cacheDuration = 6 * time.Hour
+
+// Store service with pointer return
+func InitializeStore() *StorageService{
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "172.28.83.36:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	pong, error := redisClient.Ping(ctx).Result()
+	if error != nil{
+		panic(fmt.Sprintf("Error init redis: %v", error))
+	}
+
+	fmt.Printf("\nRedis Started successfully: pong message = {%s}", &pong)
+	storeService.redisClient = redisClient
+
+	return storeService
+}
+
