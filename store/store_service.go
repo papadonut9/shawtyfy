fed83580d88d2674f2bf2a1670e8647f6c9ef676
anchxt <47145773+papadonut9@users.noreply.github.com>
2023-08-31 06:58:10 +0530

    feat: Storage APIs

--- store/store_service.go
+++ store/store_service.go
@@ -3,10 +3,8 @@ package store
 import (
 	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"time"
-
-	"github.com/go-redis/redis"
-	// "github.com/go-redis/redis/v8"
 )
 
 // Raw wrapper
@@ -24,21 +22,39 @@ var (
 const cacheDuration = 6 * time.Hour
 
 // Store service with pointer return
-func InitializeStore() *StorageService{
+func InitializeStore() *StorageService {
 	redisClient := redis.NewClient(&redis.Options{
-		Addr: "172.28.83.36:6379",
+		Addr:     "172.28.83.36:6379",
 		Password: "",
-		DB: 0,
+		DB:       0,
 	})
 
-	pong, error := redisClient.Ping(ctx).Result()
-	if error != nil{
-		panic(fmt.Sprintf("Error init redis: %v", error))
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init redis: %v", err))
 	}
 
-	fmt.Printf("\nRedis Started successfully: pong message = {%s}", &pong)
+	fmt.Printf("\nRedis Started successfully: pong message = {%s}", pong)
 	storeService.redisClient = redisClient
 
 	return storeService
 }
 
+// Storage APIs
+func saveUrlMapping(shortUrl string, originalUrl string, userid string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, cacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed Saving key url | Error: %v - Shorturl: %s\n", err, shortUrl, originalUrl))
+
+	}
+}
+
+func retrieveInitialUrl(shortUrl string) string {
+	res, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - Shorturl: %s\n", err, shortUrl))
+	}
+
+	return res
+}
