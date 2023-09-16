b49bf5c53638a56e390bd0dcf519faf039509cd8
anchxt <anchitmhatre+shawtyfy@pm.me>
2023-09-16 13:30:28 +0530

    defined handler stubs

---
+++ handler/handlers.go
@@ -0,0 +1,12 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+// Handler stubs
+func createShortUrl(c* gin.Context){
+	// TODO: add implementation
+}
+
+func handleShortUrlredirect(c* gin.Context){
+	// TODO: add implementation
+}
