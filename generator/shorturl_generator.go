8b67528aa848fae7d40fd397cb20bc5fdf9322af
anchxt <anchitmhatre+shawtyfy@pm.me>
2023-09-16 12:48:18 +0530

    feat: Encoding implementation

--- generator/shorturl_generator.go
+++ generator/shorturl_generator.go
@@ -16,4 +16,14 @@ func sha256Of(input string) []byte{
 	return algorithm.Sum(nil)
 }
 
-
+// Base58 encoding implemented
+// Using bitcoin style encoding to make the text less ambiguous
+func base58Encode(bytes []byte) string{
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil{
+		fmt.Sprintf(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
